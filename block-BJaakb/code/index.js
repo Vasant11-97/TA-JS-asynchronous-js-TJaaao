632851b252d6c016d9d54f20b876d98484f912c6
Vasant Saraswat <vasant.saraswat@gmail.com>
2021-12-01 16:45:51 +0530

    Assignment of promises is completed

---
+++ block-BJaakb/code/index.js
@@ -0,0 +1,23 @@
+// ## XMLHttpRequest + Promise
+
+// - Create a function named `fetch` which can accept one parameter an `url` and returns a promise.
+// - Use `XMLHttpRequest` to make a network request using the `url` from parameter
+// - When the data is loaded resolve the promise with the value
+// - If there is any issue loading data reject the promise with an error message
+
+function fetch(url) {
+  return new Promise((resolve, reject) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onload = () =>
+      setTimeout(() => resolve(JSON.parse(xhr.response)), 1000);
+    xhr.onerror = () => reject('Something went wrong');
+    xhr.send();
+  });
+}
+
+let data = fetch(`https://api.github.com/users/getify`)
+  .then((data) => {
+    console.log(data.name);
+  })
+  .catch((error) => alert('Check Your Internet connection'));
